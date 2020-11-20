c42349c53876e6bc07abc96c6d7a6b2c6ac8e8bc
chetan-rote <55949445+chetan-rote@users.noreply.github.com>
2020-11-20 17:46:54 +0530

    [Chetan]Add. Table for employee payroll.

--- PayrollServiceCRUDOperation.sql
+++ PayrollServiceCRUDOperation.sql
@@ -2,3 +2,12 @@
 Creating Payroll service database*/
 create database payroll_service
 use payroll_service
+
+/*UC2 Creating table */
+create table employee_payroll
+(
+id int identity(1,1) not null,
+name varchar(25) not null,
+salary money not null,
+start_date date not null
+)
