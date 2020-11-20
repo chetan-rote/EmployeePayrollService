92dc15c3452dc644b01c75092a1f66ae9e729b2a
chetan-rote <55949445+chetan-rote@users.noreply.github.com>
2020-11-20 18:17:23 +0530

    [Chetan]Add. Retrieving data from table using select command.

--- PayrollServiceCRUDOperation.sql
+++ PayrollServiceCRUDOperation.sql
@@ -17,4 +17,6 @@ insert into employee_payroll values
 ('Bill',100000.00,'2018-01-03'),
 ('Terissa',200000.00,'2019-11-13'),
 ('Charlie',300000.00,'2020-05-21');
+
+/*UC4 Retrieving Data from table*/
 select * from employee_payroll
